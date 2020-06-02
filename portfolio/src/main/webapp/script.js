29583658842075b51dbc0bc2b819078cad0d9121
ade-ademiluyi <a.aademiluyi99@gmail.com>
2020-06-02 17:00:32 +0000

    Use fetch() to request data from the server

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -32,3 +32,40 @@ function randomizeImage() {
   imageContainer.innerHTML = '';
   imageContainer.appendChild(imgElement);
 }
+
+/**
+ * Fetches my name from the server and adds it to the DOM.
+ */
+function getName() {
+  console.log('Fetching his name.');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/data');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addNameToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addNameToDom() function.
+  textPromise.then(addNameToDom);
+}
+
+/** Adds a name to the DOM. */
+function addNameToDom(name) {
+  console.log('Adding name to dom: ' + name);
+
+  const NameContainer = document.getElementById('name-container');
+  NameContainer.innerText = name;
+}
